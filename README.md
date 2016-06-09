ef25f1f44faddbf4e51d125c7df37b22532c0f38
Andrea Sghedoni <andre.sghedoni@gmail.com>
2016-06-09 11:48:22 +0200

    first commit project; uploaded structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# Project of Sistem Mobile @ Alma Mater Studiorum
---
+++ app/src/test/java/com/sm/app/wifi/sghedoni/andrea/wifism/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.sm.app.wifi.sghedoni.andrea.wifism;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
